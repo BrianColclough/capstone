550bcd1f354163ef04b0320530cb6edc9f89912d
Brian Colclough <brianmcolclough@gmail.com>
2022-02-24 16:42:57 -0500

    login function on local database

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: [true, "Firstname is required"] },
+  lastName: { type: String, required: [true, "Lastname is required"] },
+  userName: { type: String, required: [true, "Username is required"] },
+  email: {
+    type: String,
+    required: [true, "Password is required"],
+    unique: [true, "This email has been used"],
+  },
+  password: { type: String, required: [true, "Password is required"] },
+});
+
+module.exports = mongoose.model("User", userSchema);
