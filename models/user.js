1927a8fc8279e175ddd0137ab566ca0093efabf6
Rachel Altman <rachel8222@gmail.com>
2022-02-27 15:14:58 -0500

    Removed extra space

--- models/user.js
+++ models/user.js
@@ -1,9 +1,13 @@
+const mongoose = require('mongoose');
 const userSchema = new Schema({
     firstName: {type: String, required: [true, 'First name is required']},
     lastName: {type: String, required: [true, 'Last name is required']},
     email: { type: String, required: [true, 'Email address is required'], 
              unique: [true, 'This email address has been used'] },
+    username: {type: String, required: [true, 'username is required']},
     password: { type: String, required: [true, 'Password is required'] },
+    pswrepeat: { type: String, required: [true, 'Password is required'] },
 }
+
 );
 module.exports = mongoose.model('User', userSchema);
