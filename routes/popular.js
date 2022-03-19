de223933c85367e35f9cc1a9583a709bc055952e
Brian Colclough <brianmcolclough@gmail.com>
2022-03-19 13:39:50 -0400

    style fixes

--- routes/popular.js
+++ routes/popular.js
@@ -1,54 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fetch = require('node-fetch');
-var request = require('request');
+const fetch = require("node-fetch");
+var request = require("request");
 
-const apiKey = 'b85b3c13a595dcf1d03f1878600fb10e';
+const apiKey = "b85b3c13a595dcf1d03f1878600fb10e";
 const urls = {
-  nowPlaying: 'https://api.themoviedb.org/3/movie/now_playing?api_key=',
-  popular: 'https://api.themoviedb.org/3/movie/popular?api_key=',
-  top_rated: 'https://api.themoviedb.org/3/movie/top_rated?api_key=',
-  upcoming: 'https://api.themoviedb.org/3/movie/upcoming?api_key=',
-  singleMovieInfo: 'https://api.themoviedb.org/3/movie/',
-  movieInfobyTitle: 'https://api.themoviedb.org/3/search/movie?api_key=',
-  youtubeVideo: 'https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key='
+  nowPlaying: "https://api.themoviedb.org/3/movie/now_playing?api_key=",
+  popular: "https://api.themoviedb.org/3/movie/popular?api_key=",
+  top_rated: "https://api.themoviedb.org/3/movie/top_rated?api_key=",
+  upcoming: "https://api.themoviedb.org/3/movie/upcoming?api_key=",
+  singleMovieInfo: "https://api.themoviedb.org/3/movie/",
+  movieInfobyTitle: "https://api.themoviedb.org/3/search/movie?api_key=",
+  youtubeVideo: "https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=",
 };
 
-
 // Index Route
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const popular = urls.popular + apiKey;
     // load initial main movie
     const response = await fetch(popular);
     const parsedData = await response.json();
-    res.render('index', { parsedData });
+    res.render("index", { parsedData });
   } catch (e) {
     throw e;
   }
 });
 
-
-
-/**
 // SHOW- Show info about one movie
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const ID = req.params.id;
     const movieInfobyTitle = `${urls.movieInfobyTitle + apiKey}&query=${ID}`;
     const response = await fetch(movieInfobyTitle);
     const foundMovie = await response.json();
-    const singleMovieInfo = `${urls.singleMovieInfo +
-      foundMovie.results[0].id}?api_key=${apiKey}`;
-    const singleMovieCredits = `${urls.singleMovieInfo +
-      foundMovie.results[0].id}/credits?api_key=${apiKey}`;
+    const singleMovieInfo = `${
+      urls.singleMovieInfo + foundMovie.results[0].id
+    }?api_key=${apiKey}`;
+    const singleMovieCredits = `${
+      urls.singleMovieInfo + foundMovie.results[0].id
+    }/credits?api_key=${apiKey}`;
     const popular = urls.popular + apiKey;
-    const youtube = `http://api.themoviedb.org/3/movie/${
-      foundMovie.results[0].id
-    }/videos?api_key=${apiKey}&language=en-US`;
-    const recommendations = `https://api.themoviedb.org/3/movie/${
-      foundMovie.results[0].id
-    }/recommendations?api_key=${apiKey}&language=en-US&page=1`;
+    const youtube = `http://api.themoviedb.org/3/movie/${foundMovie.results[0].id}/videos?api_key=${apiKey}&language=en-US`;
+    const recommendations = `https://api.themoviedb.org/3/movie/${foundMovie.results[0].id}/recommendations?api_key=${apiKey}&language=en-US&page=1`;
     // singleMovieInfo
     try {
       const singleMovieRequest = await fetch(singleMovieInfo);
@@ -84,7 +78,7 @@ router.get('/:id', async (req, res) => {
     } catch (e) {
       throw e;
     }
-    res.render('popular/show', {
+    res.render("popular/show", {
       Movie: info,
       Video: video,
       Credit: credit,
@@ -97,7 +91,5 @@ router.get('/:id', async (req, res) => {
     throw e;
   }
 });
-
-*/
 
 module.exports = router;
